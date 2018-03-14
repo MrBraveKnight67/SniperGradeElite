ba8c18b3901c7e64d17e7ff1b2f4fa09493b6437
FHSLab <xxx>
2018-03-14 14:46:29 -0700

    Started Entity

--- src/main/entity.java
+++ src/main/entity.java
@@ -1,5 +1,17 @@
 package main;
 
 public abstract class entity {
+	public double posX;
+	public double posY;
+	public double radius;
+
+	public boolean collides(int tX, int tY) {
+		double diffX = Math.abs(tX-posX);
+		double diffY = Math.abs(tY-posY);
+		if(Math.sqrt((diffX*diffX + diffY*diffY)) <= radius) {
+			return true;
+		}
+		return false;
+	}
 	
 }
